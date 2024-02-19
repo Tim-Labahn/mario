fea994a32580c0a8f083f661de9533ceabad47ce
Kyrpychenko <denniskyrpychenko@gmail.com>
2024-02-19 17:00:33 +0100

    camera focus player

--- src/Managers/Renderer.vue
+++ src/Managers/Renderer.vue
@@ -1,13 +1,29 @@
 <script lang="ts" setup>
+import { computed } from "vue";
 import EntityManager from "./EntityManager";
 import EntityRenderer from "./EntityRenderer.vue";
 
-defineProps<{ entityManager: EntityManager }>();
+const props = defineProps<{ entityManager: EntityManager }>();
+
+const screenOffset = computed(
+  () => `calc(50vw - ${props.entityManager.getPlayer()?.x}px)`
+);
 </script>
 
 <template>
-  <EntityRenderer
-    v-for="entity in entityManager.getEntityList()"
-    :entity="entity"
-  ></EntityRenderer>
+  <div class="mapWrapper">
+    <div style="position: absolute" :style="{ left: screenOffset }">
+      <EntityRenderer
+        v-for="entity in entityManager.getEntityList()"
+        :entity="entity"
+      ></EntityRenderer>
+    </div>
+  </div>
 </template>
+<style scoped>
+.mapWrapper {
+  height: 100%;
+  position: absolute;
+  top: 30%;
+}
+</style>
